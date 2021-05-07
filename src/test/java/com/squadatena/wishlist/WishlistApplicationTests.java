546e852d68c432f50e0fd29ed984a7565c384e17
carol <carolina.gonzalez.alvarenga@gmail.com>
2021-05-06 22:47:59 -0300

    Squad-Atena Structure

---
+++ src/test/java/com/squadatena/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squadatena.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
